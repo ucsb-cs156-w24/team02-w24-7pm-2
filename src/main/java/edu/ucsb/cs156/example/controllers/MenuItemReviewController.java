059e71e5aed029e68578999e6b79de6c301555ff
nguyenliliana <liliananguyen@ucsb.edu>
2024-02-07 21:02:37 -0800

    ln - implemented PUT endpoint and tests

--- src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
@@ -86,4 +86,25 @@ public class MenuItemReviewController extends ApiController {
 
       return MIReview;
    }
+
+   @Operation(summary = "Update a single menu item review")
+   @PreAuthorize("hasRole('ROLE_ADMIN')")
+   @PutMapping("")
+   public MenuItemReview updateMenuItemReview(
+      @Parameter(name = "id") @RequestParam Long id,
+      @RequestBody @Valid MenuItemReview incoming) {
+
+      MenuItemReview menuItemReview = menuItemReviewRepository.findById(id)
+            .orElseThrow(() -> new EntityNotFoundException(MenuItemReview.class, id));
+      
+      menuItemReview.setItemId(incoming.getItemId());
+      menuItemReview.setReviewerEmail(incoming.getReviewerEmail());
+      menuItemReview.setStars(incoming.getStars());
+      menuItemReview.setDateReviewed(incoming.getDateReviewed());
+      menuItemReview.setComments(incoming.getComments());
+
+      menuItemReviewRepository.save(menuItemReview);
+
+      return menuItemReview;
+   }
 }
